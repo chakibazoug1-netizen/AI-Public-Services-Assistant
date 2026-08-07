075a10337f6ce27e2ca140e79e02cb90b5e769c8
chakibazoug1-netizen <chakibazoug1@gmail.com>
2026-08-07 11:45:13 +0100

    Add knowledge dictionary for document requirements

---
+++ knowledge.py
@@ -0,0 +1,42 @@
+knowledge = {
+    "passport": """
+Passport Requirements
+
+Required documents:
+- National Identity Card
+- Birth Certificate
+- Biometric Photo
+- Payment of fees
+""",
+
+    "identity card": """
+National Identity Card
+
+Required documents:
+- Birth Certificate
+- Residence Certificate
+- Biometric Photo
+""",
+
+    "birth certificate": """
+Birth Certificate
+
+Issued by the Civil Status Office.
+""",
+
+    "residence certificate": """
+Residence Certificate
+
+Issued by the municipality of residence.
+""",
+
+    "driving licence": """
+Driving Licence
+
+Required documents:
+- Medical Certificate
+- Identity Card
+- Photos
+- Payment of fees
+"""
+}
